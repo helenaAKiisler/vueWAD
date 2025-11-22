3ebbbba547f37db73f8a912a1ea956a85ddfa1d4
helenaAKiisler <helena.angela.kiisler@ut.ee>
2025-11-22 13:36:19 +0200

    adding the routes to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,3 +2,22 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '../views/MainPage.vue'
 import SignUp from '../views/SignUp.vue'
 
+const routes = [{
+    path: '/',
+    name: 'main',
+    component: MainPage
+},
+{
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
+}
+]
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
